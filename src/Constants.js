dd11f06db30e012d85b0c6c7637bafa57321e667
Panagiotis Dimopoulos <panosdim@gmail.com>
2022-03-21 09:22:55 +0200

    Change backend server URL

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 // TODO: Change to production url https://api.leaves.cc.nf/
-export const apiBaseUrl = 'https://api.leaves.cc.nf/';
+export const apiBaseUrl = 'https://api-leaves.sites.freeddns.org:9006/';
 export const currentYear = new Date().getFullYear().toString();
